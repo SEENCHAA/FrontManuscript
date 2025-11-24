ac79a901fd325f4825f9788c8ea2071989617f14
SEENCHAA <arsmir05@mail.ru>
2025-11-25 00:18:16 +0300

    ....

--- frontend/src/pages/SignDetailPage.tsx
+++ frontend/src/pages/SignDetailPage.tsx
@@ -10,7 +10,7 @@ const MINIO_BUCKET = 'manuscripts';
 const MINIO_BASE_URL = `/${MINIO_BUCKET}/`;
 
 const LOGO_URL = `${APP_BASE}british-museum-logo.svg`;
-const BACK_ICON_URL = `${APP_BASE}icons8-назад-64.png`; 
+
 const PLACEHOLDER_URL = `${APP_BASE}placeholder.jpg`; 
 
 export const SignDetailPage: React.FC = () => {
